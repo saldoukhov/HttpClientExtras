fa244f05be9a58783abb35b2a570a287f880126c
Sergey Aldoukhov <sergey_aldoukhov@intuit.com>
2014-03-07 17:42:26 -0800

    Android support

---
+++ HttpClientExtras.Android/PlatformAdapters.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HttpClientExtras
+{
+    internal class OAuthToolsPlatformAdapter : IOAuthToolsPlatformAdapter
+    {
+
+        public int GetSeed()
+        {
+            var bytes = new byte[4];
+            new RNGCryptoServiceProvider().GetNonZeroBytes(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        public string GetOAuthSignature(string signatureBase, string consumerSecret, string tokenSecret)
+        {
+            consumerSecret = Uri.EscapeDataString(consumerSecret);
+            if (!string.IsNullOrEmpty(tokenSecret))
+                tokenSecret = Uri.EscapeDataString(tokenSecret);
+            var crypto = new HMACSHA1(Encoding.UTF8.GetBytes(consumerSecret + "&" + tokenSecret));
+            var signature = HashWith(signatureBase, crypto);
+            var result = Uri.EscapeDataString(signature);
+            return result;
+        }
+
+        public string GetAzureSignature(string signatureBase, string sharedKey)
+        {
+            var crypto = new HMACSHA256(Convert.FromBase64String(sharedKey));
+            var signature = HashWith(signatureBase, crypto);
+            return signature;
+        }
+        
+        private static string HashWith(string input, HashAlgorithm algorithm)
+        {
+            var data = Encoding.UTF8.GetBytes(input);
+            var hash = algorithm.ComputeHash(data);
+            return Convert.ToBase64String(hash);
+        }
+    }
+
+
+    public static class PlatformAdapters
+    {
+        public static void Init()
+        {
+            var adapter = new OAuthToolsPlatformAdapter();
+            OAuthTools.PlatformAdapter = adapter;
+            Azure.PlatformAdapter = adapter;
+        }
+    }
+}
